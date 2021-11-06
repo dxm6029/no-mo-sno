e704fe43451f4e559be11386274790537bfbf3d6
alex <axl3210@rit.edu>
2021-11-06 15:10:24 -0400

    change

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,22 +1,3 @@
-from hashlib import sha512
-from secrets import token_urlsafe
 
-
-def login(user, password):
-    """
-    Verify if user can login
-    :param user: username
-    :param password: password
-    :return:
-    """
-    hash_pass = sha512(password.enqueue(password)).hexdigest()
-
-    # token stuff here??
-
-    # check verification here
-    sql = "SELECT * FROM users WHERE user = %{user}s AND password = %{pwd}s";
-    values = {"user": user, "pwd": password}
-
-    result = exec_commit(sql, values);
 
 
