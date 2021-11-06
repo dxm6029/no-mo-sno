80e2d59f8e76755eeada361c447cf2ff715fe891
Ainsley Ross <apr8760@rit.edu>
2021-11-06 18:10:42 -0400

    getComments-GET

--- src/server.py
+++ src/server.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_restful import Resource, Api
+
+from api.getComments import getComments
 from api.healthcheck import Healthcheck
 from api.login import Login
 from api.register import Register
+from api.comments import Comments
 
 
 app = Flask(__name__)
@@ -11,6 +14,9 @@ api = Api(app)
 api.add_resource(Healthcheck, '/')
 api.add_resource(Login, '/login')
 api.add_resource(Register, '/register')
+api.add_resource(Comments, '/comments')
+api.add_resource(getComments, '/comments/<id>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
