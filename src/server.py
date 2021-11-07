1cc7f01dd08cc9633fd82ebd19cb25ab9dfaa269
dominique-molee <dxm6029@rit.edu>
2021-11-06 22:58:01 -0400

    getJobInfo

--- src/server.py
+++ src/server.py
@@ -8,6 +8,7 @@ from api.comments import Comments
 from api.getComments import getComments
 from api.getJobs import getJobs
 from api.addJobs import addJobs
+from api.getJobInfo import getJobInfo
 
 
 app = Flask(__name__)
@@ -20,6 +21,8 @@ api.add_resource(Comments, '/comments')
 api.add_resource(getComments, '/comments/<id>')
 api.add_resource(getJobs, '/jobs')
 api.add_resource(addJobs, '/jobs')
+api.add_resource(getJobInfo, '/jobInfo')
+
 
 
 if __name__ == '__main__':
